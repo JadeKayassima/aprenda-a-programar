85bfc192bda7b05749682983224c2335dd8d410d
Jade Kayassima <jade_27_02@hotmail.com>
2022-08-24 01:41:47 -0300

    alteracao arquivo

--- tarefa-dia-2/pratica-1.js
+++ tarefa-dia-2/pratica-1.js
@@ -1,11 +1,29 @@
-nome = "Jade"
-email = "jade_27_02@hotmail.com"
-profissao = "Engenheira"
-idade = 27
-lugarDosSonhos = "Japão"
+// 
 
-console.log(nome)
-console.log(email)
-console.log(profissao)
+
+let nome = "Jade"
+let email = "jade_27_02@hotmail.com"
+let profissao = "Engenheira"
+let idade = 27
+let lugarDosSonhos = "Japão"
+let texto1 = "Olá"
+let texto2 = "somos aqui da agência de turismo Viagem dos Sonhos. Estamos te escrevendo este email, pois acabamos de confirmar as compras das passagens aéreas para o"
+let texto3 = "Confirma pra gente alguns dados?"
+let texto4 = "A sua idade é?"  
+let texto5 = "Você é?"
+let texto6 = "O seu email é?"
+let texto7 = "Ficamos no aguardo. Muito obrigada e boa viagem! :D"
+
+console.log(texto1, nome, texto2, lugarDosSonhos)
+console.log(texto3)
+console.log(texto4)
 console.log(idade)
-console.log(lugarDosSonhos)
+
+console.log(texto5)
+console.log(profissao)
+
+console.log(texto6)
+console.log(email)
+
+console.log(texto7)
+
